bbbbefd9f9fc96cd810902e2ca6f91f7ae36c267
Abdulaziz_c5 <abdulazizbekabdusodiqov@gmail.com>
2025-02-09 09:21:24 +0500

    update

--- src/admin/dto/create-admin.dto.ts
+++ src/admin/dto/create-admin.dto.ts
@@ -1,1 +1,9 @@
-export class CreateAdminDto {}
+export class CreateAdminDto {
+    first_name:string;
+    last_name:string;
+    user_name:string;
+    phone_number:string;
+    email:string;
+    password:string;
+    confirm_password:string;
+}
