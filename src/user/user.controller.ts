d7f43e353ea297c7a9754289cde2dc5c5faa1ad9
AbdulazizbekAbdusodiqov <abdulazizbekabdusodiqov@gmail.com>
2025-03-11 13:30:27 +0500

    add user guards

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,10 @@
-import { Controller, Get, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto, UpdateUserDto } from './dto';
 import { ApiOperation } from '@nestjs/swagger';
+import { AdminGuard } from '../guards/admin.guard';
+import { UserGuard } from '../guards/user.guard';
+import { UserSelfGuard } from '../guards/user-self.guard';
 
 @Controller('user')
 export class UserController {
@@ -12,35 +15,41 @@ export class UserController {
   }
 
   @ApiOperation({ summary: 'Get all users' })
+  @UseGuards(AdminGuard)
   @Get()
   findAll() {
     return this.userService.findAll();
   }
   @ApiOperation({ summary: 'Get all entrepreneurs' })
+  @UseGuards(AdminGuard)
   @Get('entrepreneur')
   findAllEntrepreneur() {
     return this.userService.findAllEntrepreneur();
   }
-
+  
   @ApiOperation({ summary: 'Get all investors' })
+  @UseGuards(AdminGuard)
   @Get('investor')
   findAllInvestor() {
     return this.userService.findAllInvestor();
   }
-
+  
   @ApiOperation({ summary: 'Get a user by ID' })
+  @UseGuards(UserGuard, UserSelfGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.userService.findOne(+id);
   }
-
+  
   @ApiOperation({ summary: 'Update a user by ID' })
+  @UseGuards(UserGuard, UserSelfGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.update(+id, updateUserDto);
   }
-
+  
   @ApiOperation({ summary: 'Delete a user by ID' })
+  @UseGuards(AdminGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.userService.remove(+id);
